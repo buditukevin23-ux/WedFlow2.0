7402612d0609edbbc3b55a32989317ac02ee028c
buditukevin23-ux <buditukevin23@gmail.com>
2026-07-31 23:32:53 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const cacheName = "wedflow-v1";
+
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./firebase.js"
+];
+
+
+self.addEventListener("install", (event)=>{
+
+    event.waitUntil(
+
+        caches.open(cacheName)
+        .then(cache=>{
+
+            return cache.addAll(filesToCache);
+
+        })
+
+    );
+
+});
+
+
+
+self.addEventListener("fetch",(event)=>{
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response=>{
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
